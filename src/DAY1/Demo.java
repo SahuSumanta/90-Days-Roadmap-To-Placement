90ced85c329ca29017d4ada35803512a8812cd54
SahuSumanta <smsahu9@gmail.com>
2022-04-23 11:40:03 -0400

    90 Days Roadmap To Placement

---
+++ src/DAY1/Demo.java
@@ -0,0 +1,7 @@
+package DAY1;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Aumnta");
+    }
+}
---
+++ src/DAY3/Practice.java
@@ -0,0 +1,7 @@
+package DAY3;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("sumanta");
+    }
+}
