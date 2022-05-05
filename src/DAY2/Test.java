3dbf93e31a11dd401ba94b138e168793b846291e
SahuSumanta <smsahu9@gmail.com>
2022-05-05 22:22:06 +0530

    Java Test file deleted

--- src/DAY2/Test.java
+++
@@ -1,7 +0,0 @@
-package DAY2;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("sumanta");
-    }
-}
--- src/DAY3/Practice.java
+++
@@ -1,7 +0,0 @@
-package DAY3;
-
-public class Practice {
-    public static void main(String[] args) {
-        System.out.println("sumanta");
-    }
-}
