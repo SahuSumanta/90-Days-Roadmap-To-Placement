6bc2d448dfdbcc81b0cc19c6d787f04954f70c17
SahuSumanta <smsahu9@gmail.com>
2022-05-16 12:39:00 +0530

    Merge Interval

---
+++ src/DAY2/MergeInterval.java
@@ -0,0 +1,38 @@
+package DAY2;
+
+import java.util.*;
+
+/**
+ * MergeInterval
+ */
+public class MergeInterval {
+
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        
+        if(intervals.length == 0 || intervals == null) 
+            return res.toArray(new int[0][]);
+        
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        for(int[] i : intervals) {
+            if(i[0] <= end) {
+                end = Math.max(end, i[1]);
+            }
+            else {
+                res.add(new int[]{start, end});
+                start = i[0];
+                end = i[1];
+            }
+        }
+        res.add(new int[]{start, end});
+       return res.toArray(new int[0][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1,3},{2,6},{8,10},{15,18}};
+    }
+}
