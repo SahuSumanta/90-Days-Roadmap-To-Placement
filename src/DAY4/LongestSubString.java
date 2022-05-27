c15765e46cb00b82b1403ea01eb4df9b302c459b
SahuSumanta <smsahu9@gmail.com>
2022-05-28 01:47:30 +0530

    Length of Longest Substring without any Repeating Character

---
+++ src/DAY4/LongestSubString.java
@@ -0,0 +1,29 @@
+package DAY4;
+
+import java.util.HashMap;
+
+public class LongestSubString {
+    public static void main(String[] args) {
+       String s = "pwwkew";
+       System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int  left = 0, right = 0;
+        int n = s.length();
+        int len = 0;
+
+        while (right < n) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(map.get(s.charAt(right)) +1, left);
+            }
+
+            map.put(s.charAt(right), right);
+
+            len = Math.max(len, right - left+1);
+            right++;
+        }
+        return len;
+    }
+}
